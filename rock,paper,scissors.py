b687c4e78913e429fc55c9d1d4ffb49c7992d5d7
sujith sk <sujith.skanchan986@gmail.com>
2025-08-04 14:21:38 +0530

    Added a score tracker

--- rock,paper,scissors.py
+++ rock,paper,scissors.py
@@ -1,16 +1,31 @@
 import random
 
 def check_winner(user, comp):
+    global win, draw, lost
     if user == comp or (user[0] == comp[0] and len(user) == 1):
-        print(f"It's a draw! Computer chose {comp}")
+        print(f"It's a draw! Computer chose {comp}\n")
+        draw = True
+        win = False
+        lost = False
+
     elif (user in ["rock", "r"] and comp in ["scissors", "s"]) or \
          (user in ["paper", "p"] and comp in ["rock", "r"]) or \
          (user in ["scissors", "s"] and comp in ["paper", "p"]):
-        print(f"You win! Computer chose {comp}")
+        print(f"You win! Computer chose {comp}\n")
+        win = True
+        draw = False
+        lost = False
+
     else:
-        print(f"You lost! Computer chose {comp}")
+        print(f"You lost! Computer chose {comp}\n")
+        lost = True
+        win = False
+        draw = False
 
 options = ["rock", "paper", "scissors", "r", "p", "s"]
+win_count = 0
+lost_count = 0
+draw_count = 0
 
 while True:
     user = input("\nEnter your choice (rock, paper, or scissors). Type 'q' to quit: ").lower()
@@ -23,3 +38,16 @@ while True:
     else:
         comp = random.choice(["rock", "paper", "scissors"])  # only full names for comp
         check_winner(user, comp)
+        if (win == True):
+            win_count += 1
+            print("YOUR SCORE: ", win_count)
+        elif(lost == True):
+            lost_count += 1
+        elif(draw == True):
+            draw_count += 1
+        
+    print("WIN LOSS DRAW COUNTER: ")
+    print("WINS: ", win_count)
+    print("LOSS: ", lost_count)
+    print("DRAW: ", draw_count)
+    
